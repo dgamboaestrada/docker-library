0a8432189c94bc9cc6b6cae09d178e5878cf3727
Daniel Gamboa Estrada <dgamboaestrada@gmail.com>
2018-04-13 22:33:59 -0500

    nginx: Set default root path and upstream socket to php-fpm nginx

--- nginx/php-fpm/entrypoint.sh
+++ nginx/php-fpm/entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+if [ -z $ROOT ]; then
+    ROOT='/var/www/html'
+fi
+echo "Root value: $ROOT"
+
+if [ -z $UPSTREAM ]; then
+    UPSTREAM='php'
+fi
+echo "Upstream value: $UPSTREAM"
+
 echo $(envsubst '${ROOT},${UPSTREAM}' < /etc/nginx/conf.d/default.conf) > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
 exec "$@"
