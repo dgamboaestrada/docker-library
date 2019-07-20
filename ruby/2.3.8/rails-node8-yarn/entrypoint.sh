fdf2ebd9adffabcc92457a99fb9ff33d861f4129
Daniel Gamboa Estrada <dgamboaestrada@gmail.com>
2019-07-19 20:05:47 -0500

    ruby: Add rails image with ruby 2.3.8, node8 and yard

--- ruby/2.3.8/rails-node8-yarn/entrypoint.sh
+++ ruby/2.3.8/rails-node8-yarn/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export BUNDLE_PATH=/myapp/vendor/bundle
 if [ -z $RAILS_ENV ]; then
     APP_ENV=development
 else
@@ -14,12 +13,12 @@ fi
 if [ -z $RDS_PORT ]; then
     RDS_PORT=5432
 fi
-if [ -z $SETUP ]; then
-    SETUP=true
-fi
+
+echo "info: Default environmnents vars: RAILS_ENV: $RAILS_ENV, PORT: $PORT, RDS_HOST: $RDS_HOST, RDS_PORT: $RDS_PORT, BUNDLE_PATH: $BUNDLE_PATH"
 
 # It's created a new proyect if not existe a proyect.
 if [ ! "$(ls -A /myapp)" ]; then
+    echo "Creating a new rails proyect..."
     gem install rails
     rails new /myapp -f
 fi
@@ -28,16 +27,20 @@ cd /myapp
 # Delete server pid if exist.
 set -e
 if [ -f /myapp/tmp/pids/server.pid ]; then
-  rm /myapp/tmp/pids/server.pid
+    echo "Deleting pid server..."
+    rm /myapp/tmp/pids/server.pid
 fi
 
-if $SETUP; then
-    # Install dependencies.
-    bundle install
-    yarn install
+# Install dependencies.
+echo "Instaling gems..."
+bundle install
+echo "Instaling yarn dependencies..."
+yarn install
 
+if [ "$APP_ENV" == "development" ]; then
     # Compiles webpack bundles if has the webpacker gem.
     if grep -q "webpacker" Gemfile; then
+        echo "Webpack compiling..."
         bundle exec rake webpacker:clobber
         bundle exec rake webpacker:compile
     fi
@@ -46,22 +49,22 @@ fi
 #
 if nc -vz $RDS_HOST $RDS_PORT; then
     # Run migration fo test environment.
-    if [ "$APP_ENV" != "production" ]; then
+    if [ "$APP_ENV" == "development" ]; then
         export RAILS_ENV=test;
-        if $SETUP; then
-            bundle exec rake db:create
-        fi
+        bundle exec rake db:create
         bundle exec rake db:migrate
     fi
 
     # Run migration.
     export RAILS_ENV=$APP_ENV
-    if $SETUP; then
+    if [ "$APP_ENV" == "development" ]; then
         bundle exec rake db:create
     fi
     bundle exec rake db:migrate
 
     # Run the server.
     bundle exec rails server -p $PORT -b 0.0.0.0
-    exec "$@"
+else
+    echo "RDS HOST $RDS_HOST with port $RDS_PORT not found."
 fi
+exec "$@"
